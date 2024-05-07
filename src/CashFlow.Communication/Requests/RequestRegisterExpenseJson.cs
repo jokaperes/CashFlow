6fe2a77144f91b91a4f0bf687194d9cb33dc6d58
jokaperes <jokaperes@gmail.com>
2024-05-06 22:56:27 -0300

    Adicionar arquivos de projeto.

---
+++ src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Communication.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashFlow.Communication.Requests
+{
+    public class RequestRegisterExpenseJson
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime Date {  get; set; }
+
+        public decimal Amount { get; set; }
+        public PaymentType PaymentType {  get; set; }
+    }
+}
