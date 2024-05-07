cb60cddcef99828b1c4ced94ef28b350f972ff61
jokaperes <jokaperes@gmail.com>
2024-05-06 23:33:19 -0300

    adicionando excecoes

--- src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -1,9 +1,4 @@
 ﻿using CashFlow.Communication.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CashFlow.Communication.Requests
 {
