7bbbe28b98b87e8186eab34302b905c82a51c482
jokaperes <jokaperes@gmail.com>
2024-06-19 01:40:17 -0300

    adding nuget package fluent validation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -14,30 +14,20 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
         private void Validate(RequestRegisterExpenseJson request)
         {
+
+            var validator = new RegisterExpenseValidator();
+
+            var validationResult = validator.Validate(request);
+
             
-            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-            if (titleIsEmpty)
-            {
-                throw new ArgumentException("The title is required.");
-            }
 
-            if(request.Amount <= 0)
+            if (validationResult.IsValid == false)
             {
-                throw new ArgumentException("The amount must be greater than zero");
-            }
+                var errorMessages = validationResult.Errors.Select(f => f.ErrorMessage).ToList();
 
-           var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if (result > 0) 
-            {
-                throw new ArgumentException("Expenses must be in the past, not in the future");
+                throw new ArgumentException(errorMessages);
             }
-
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-            if (paymentTypeIsValid == false)
-            {
-                throw new ArgumentException("Payment type is not valid, must be between 0 and 4");
-            }
-
+                    
         }
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using CashFlow.Communication.Requests;      
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+    {
+        public RegisterExpenseValidator()
+        {
+            RuleFor(expense => expense.Title)
+                .NotEmpty()
+                .WithMessage("The title is required.");
+
+            RuleFor(expense => expense.Amount)
+                .GreaterThan(0)
+                .WithMessage("The amount must be greater than 0.");
+
+            RuleFor(expense => expense.Date)
+                .LessThanOrEqualTo(DateTime.UtcNow)
+                .WithMessage("Expenses must be in the past, not in the future.");
+
+            RuleFor(expense => expense.PaymentType)
+                .IsInEnum()
+                .WithMessage("Payment type is not valid, must be between 0 and 4");
+        }
+    }
+}
